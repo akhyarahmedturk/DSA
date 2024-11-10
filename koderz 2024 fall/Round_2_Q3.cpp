7b7a6059bb5c3e8bef7c8a8a1d86691e8dd4a4d6
akhyarahmedturk <akhyarahmedturk@gmail.com>
2024-11-10 07:12:30 +0500

    round 2

--- koderz 2024 fall/Round_2_Q3.cpp
+++ koderz 2024 fall/Round_2_Q3.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 // Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.
 
-//Node struct is defined as follows:
-    struct Node {
-        int data;
-        Node* left;
-        Node* right;
-    };
+//TreeNode struct is defined as follows:
+struct TreeNode{
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+};
 
-bool validateBST(Node* root, long long minVal, long long maxVal){
+bool validateBST(TreeNode* root, long long minVal, long long maxVal){
     if (!root)
         return true;
 
@@ -19,6 +19,6 @@ bool validateBST(Node* root, long long minVal, long long maxVal){
 
     return validateBST(root->left, minVal, root->data) && validateBST(root->right, root->data, maxVal);
 }
-bool checkBST(Node* root){
+bool checkBST(TreeNode* root){
     return validateBST(root, -1, 10000000);
 }
